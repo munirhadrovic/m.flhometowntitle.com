707b822a98e559cac951c82575fda8d3a4407b5a
anes89cok <anes89cok@gmail.com>
2015-05-29 11:27:30 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="870 W. Hickpochee Ave. Suite 1600 LaBelle, FL 33935"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Florida";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="Fl"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=8632717191;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
